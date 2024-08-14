bdb7b693a425d07f43295da8a30293e4d3f81b57
ares.popal <ares.popal@student.howest.be>
2024-08-14 23:04:53 +0200

    Add sort functionality and unify code in searches

--- Services/Services.cs
+++ Services/Services.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MTG_Project.Models;
 using MTG_Project.ModelsDTO;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace MTG_Project.Services
 {
@@ -9,54 +10,51 @@ namespace MTG_Project.Services
         readonly MyDBContext dbContext = new();
 
         public Services() { }
-
-        //GetCards(string sortOrder, string cardLetters, string artistLetters, int cardType, int setCode, string rarityCode, int[] colors, bool colorsExact, int pageNumber, int pageSize))
-
-        public IList<Card> GetAllCards(int pageNumber, int pageSize)
+        
+        /*public IList<Card> GetCards(string sortOrder = "Id", string cardLetters = "", string artistLetters = "", int cardType = 0, int setCode = 0, string rarityCode = "", int[] colors = null, bool colorsExact = false, int pageNumber, int pageSize)
         {
-            IQueryable<Card> query = dbContext.Cards;
 
-            /*
-            query = Util.OrderBySortOrder(query, sortOrder);
+        }*/
 
-            return query
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
-                .ToList();
-            */
-
-            query = query.OrderBy(c => c.Id);
+        public IList<Card> GetAllCards(int pageNumber, int pageSize, string sortOrder)
+        {
+            IQueryable<Card> query = Util.OrderBySortOrder(dbContext.Cards, sortOrder);
 
             return Util.Paginate(query, pageNumber, pageSize).ToList();
         }
 
-        public IList<Card> GetAllCardsWith(string letters, int pageNumber, int pageSize)
+        public IList<Card> GetAllCardsWith(string letters, int pageNumber, int pageSize, string sortOrder)
         {
             Console.WriteLine(letters);
-            return dbContext.Cards.Where(x => x.Name.Contains(letters))
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
-                .ToList();
+            IQueryable<Card> query = Util.OrderBySortOrder(dbContext.Cards, sortOrder);
+
+            query = query.Where(x => x.Name.Contains(letters));
+
+            return Util.Paginate(query, pageNumber, pageSize).ToList();
         }
 
-        public IList<Card> GetAllCardsWithArtist(string artistLetters, int pageNumber, int pageSize)
+        public IList<Card> GetAllCardsWithArtist(string artistLetters, int pageNumber, int pageSize, string sortOrder)
         {
             Console.WriteLine(artistLetters);
-            return dbContext.Cards.Where(c => c.Artist.FullName.Contains(artistLetters))
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
-                .ToList();
+
+            IQueryable<Card> query = Util.OrderBySortOrder(dbContext.Cards, sortOrder);
+
+            query = query.Where(c => c.Artist.FullName.Contains(artistLetters));
+
+            return Util.Paginate(query, pageNumber, pageSize).ToList();
         }
         
-        public IList<Card> GetAllCardsWithAndWithArtist(string cardLetters, string artistLetters, int pageNumber, int pageSize)
+        public IList<Card> GetAllCardsWithAndWithArtist(string cardLetters, string artistLetters, int pageNumber, int pageSize, string sortOrder)
         {
             Console.WriteLine(cardLetters, artistLetters);
-            return dbContext.Cards
+
+            IQueryable<Card> query = Util.OrderBySortOrder(dbContext.Cards, sortOrder);
+
+            query = query
                 .Where(c => c.Artist.FullName.Contains(artistLetters))
-                .Where(c => c.Name.Contains(cardLetters))
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
-                .ToList();
+                .Where(c => c.Name.Contains(cardLetters));
+
+            return Util.Paginate(query, pageNumber, pageSize).ToList();
         }
 
         public IQueryable GetCardsByArtist(int idArtist)
--- Services/Util.cs
+++ Services/Util.cs
@@ -1,29 +1,25 @@
 ﻿using MTG_Project.Models;
+using System.Linq.Expressions;
 
 namespace MTG_Project.Services
 {
     public class Util
     {
-        public static IQueryable<Card> OrderBySortOrder(IQueryable<Card> query, string sortOrder = "")
+        public static IQueryable<Card> OrderBySortOrder(IQueryable<Card> query, string sortOrder)
         {
-            switch (sortOrder)
-            {
-                case "name":
-                    query = query.OrderBy(c => c.Name);
-                    break;
-                case "artist":
-                    query = query.OrderBy(c => c.Artist.FullName);
-                    break;
-                default:
-                    query = query.OrderBy(c => c.Id);
-                    break;
-            }
-            return query;
+            var parameter = Expression.Parameter(typeof(Card), "c");
+            var property = Expression.Property(parameter, sortOrder);
+            var lambda = Expression.Lambda(property, parameter);
+
+            var orderByMethod = typeof(Queryable).GetMethods()
+                .First(m => m.Name == "OrderBy" && m.GetParameters().Length == 2)
+                .MakeGenericMethod(typeof(Card), property.Type);
+
+            return (IQueryable<Card>)orderByMethod.Invoke(null, parameters: [query, lambda]);
         }
 
         public static IQueryable<Card> Paginate(IQueryable<Card> query, int pageNumber, int pageSize)
         {
-            Console.WriteLine("Succes");
             return query
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize);
